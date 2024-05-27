094501f8b453c65dbc06912c2b4241e0aa473b68
mltony <anton.malykh+0@gmail.com>
2024-05-27 14:38:47 -0700

    Bump and sleep for chrome

--- buildVars.py
+++ buildVars.py
@@ -25,7 +25,7 @@ addon_info = {
 	# Translators: Long description to be shown for this add-on on add-on information from add-ons manager
 	"addon_description": _("""Improved navigate by word and word selection commands."""),
 	# version
-	"addon_version": "2.0dev",
+	"addon_version": "2.0",
 	# Author(s)
 	"addon_author": "Tony Malykh <anton.malykh@gmail.com>",
 	# URL for the add-on documentation support
